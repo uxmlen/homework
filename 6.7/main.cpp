711bf63f11c641e8b9515f681e94eeaedfc4b85a
uxmlen <example@example.com>
2023-01-30 16:36:53 +0300

    6.7

--- 6.7/main.cpp
+++ 6.7/main.cpp
@@ -2,14 +2,15 @@
 #include <array>
 
 #include "audio_speaker.h"
-#include "fridge.h"
 #include "tv_station.h"
+#include "phone.h"
+#include "fridge.h"
 
 
 int main(void)
 {
     int battery_type = 1;
-    size_t count = 5;
+    size_t count = 8;
     char flag = 'y';
     Electronic *items[count];
 
@@ -18,6 +19,9 @@ int main(void)
     items[2] = new Fridge("Холодильник", 10000, 30, 20, 4);
     items[3] = new Fridge("Холодильник 2", 15000, 30, 20, 4);
     items[4] = new TVStation("TV station new", 7000, 30, 40, 300);
+    items[5] = new TVStation("TV 2 station", 6000, 20, 40, 310);
+    items[6] = new Phone("Xiamo", 6000, battery_type, 35, 'A');
+    items[7] = new Phone("Iphone", 6000, battery_type, 35, 'I');
 
     while(flag == 'y') {
         int choice;
--- 6.7/phone.cpp
+++ 6.7/phone.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-#include "audio_speaker.h"
+#include "phone.h"
 
-void AudioSpeaker::showInfo() const
+void Phone::showInfo() const
 {
     std::cout << "Название: " << name_ << std::endl
               << "Тип аккумулятора: " << type_battery_ << std::endl
               << "Время работы аккумулятора?: " << working_hours << std::endl
               << "Потребление: " << consumation_ << std::endl
+              << "OS: " << (OS_ == 'I' ? "Iphone" : "Android") << std::endl
               << "Цена: " << price_ << std::endl;
-}
+
+};
